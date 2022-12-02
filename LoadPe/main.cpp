6d0d2e098ff4110ba4b0acaea52ad5d0d906b43e
tangtang <2998888000@qq.com>
2022-12-02 18:38:26 +0800

    132

--- LoadPe/main.cpp
+++ LoadPe/main.cpp
@@ -5,12 +5,13 @@
 
 /*
 	https://thunderjie.github.io/2019/03/27/PE%E7%BB%93%E6%9E%84%E8%AF%A6%E8%A7%A3/
+	https://blog.csdn.net/zhyulo/article/details/85717711
 */
 
 int main()
 {
 	_IMAGE_DOS_HEADER;
-	_IMAGE_NT_HEADERS;
+	_IMAGE_NT_HEADERS64;
 	
 	std::filesystem::path file_name = kProjectSourceDir"build_windows/bin/Debug/Test.exe";
 	auto file_size = std::filesystem::file_size(file_name);
@@ -20,7 +21,11 @@ int main()
 	is.read(buffer, file_size);
 	is.close();
 
+	constexpr auto pe_size = sizeof(_IMAGE_NT_HEADERS64);
+
 	auto dos = (_IMAGE_DOS_HEADER*)buffer;
+	auto pe = (_IMAGE_NT_HEADERS64*)(buffer + dos->e_lfanew);
+	auto sec_heard = (_IMAGE_SECTION_HEADER*)(buffer + dos->e_lfanew + pe_size);
 
 	return 0;
 }
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+static int number = 10;
+static const char* str = "hello";
+
 int main()
 {
+	static char* buf = new char[10];
+
+	std::cout << str << std::endl;
 	return 0;
 }
